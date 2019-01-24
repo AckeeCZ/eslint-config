840d4eea5f04d90d3ad60b18f116732568510918
Jiří Čermák <jiri.cermak@ackee.cz>
2019-01-24 16:28:06 +0100

    Update dependencies

--- backend.js
+++ backend.js
@@ -1,24 +1,20 @@
 module.exports = {
     env: {
-        "node": true,
+        node: true,
     },
-    extends: [
-        "./index.js"
-    ],
+    extends: ['./index.js'],
     rules: {
         /**
-         * REASON: Some 3rd party libraries use dangles for functions names. 
-         *  See. https://gitlab.ack.ee/Web-public/eslint-config-ackee/issues/4
+         * REASON: Some 3rd party libraries use dangles for functions names.
          */
         // https://eslint.org/docs/rules/no-underscore-dangle
-        "no-underscore-dangle": "off",
+        'no-underscore-dangle': 'off',
 
         /**
          * REASON: In case of using custom NODE_PATH (common case for backend) linter doesn't recognize
-         *  the path and complains about unresolved paths. 
-         *  See https://gitlab.ack.ee/Web-public/eslint-config-ackee/issues/1
+         *  the path and complains about unresolved paths.
          */
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-unresolved.md
-        "import/no-unresolved": "off"
-    }
-}
+        'import/no-unresolved': 'off',
+    },
+};
