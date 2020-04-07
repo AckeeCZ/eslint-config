167c4bbec4cbe03dc4772c74b7306e6cc40bf3ae
Jiří Zdvomka <zdvomka.j@hotmail.cz>
2020-04-07 17:39:27 +0200

    ✨ add prop-types rule

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["react-app"],
   rules: {
-    "react-hooks/exhaustive-deps": "off"
-  }
+    "react-hooks/exhaustive-deps": "off",
+    "react/prop-types": [1, { ignore: ["styles", "rules"] }],
+  },
 };
