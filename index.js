e300b80bf270a98e8f44d0e7402f71c8330d7f33
Jiří Zdvomka <zdvomka.j@hotmail.cz>
2020-04-07 17:43:12 +0200

    ✨ add no-unused-prop-types

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ module.exports = {
   rules: {
     "react-hooks/exhaustive-deps": "off",
     "react/prop-types": [1, { ignore: ["styles", "rules"] }],
+    "react/no-unused-prop-types": [
+      1,
+      {
+        customValidators: [],
+        skipShapeProps: true,
+      },
+    ],
   },
 };
